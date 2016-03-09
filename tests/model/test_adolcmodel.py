80a49a64d41a0b3cfdfd025ae1bb54968ca9d4c0
Dominique Orban <dominique.orban@gmail.com>
2016-03-09 17:46:41 -0500

    pytest: skip irrelevant tests

--- tests/model/test_adolcmodel.py
+++ tests/model/test_adolcmodel.py
@@ -3,8 +3,11 @@
 from unittest import TestCase
 from helper import *
 import numpy as np
+import pytest
 
-if not module_missing('adolc'):
+adolc_missing = pysparse_missing = scipy_missing = False
+
+try:
     from nlp.model.adolcmodel import BaseAdolcModel, SparseAdolcModel
 
     class AdolcRosenbrock(BaseAdolcModel):
@@ -12,6 +15,9 @@ if not module_missing('adolc'):
 
         def obj(self, x, **kwargs):
             return np.sum(100 * (x[1:] - x[:-1]**2)**2 + (1 - x[:-1])**2)
+
+    class SparseRosenbrock(SparseAdolcModel, AdolcRosenbrock):
+        pass
 
     class AdolcHs7(BaseAdolcModel):
         """Problem #7 in the Hock and Schittkowski collection."""
@@ -22,131 +28,160 @@ if not module_missing('adolc'):
         def cons(self, x, **kwargs):
             return np.array([(1 + x[0]**2)**2 + x[1]**2])
 
-    class Test_AdolcRosenbrock(TestCase, Rosenbrock):
-        # Test def'd in Rosenbrock
-
-        def get_derivatives(self, model):
-            return get_derivatives_plain(model)
-
-        def setUp(self):
-            n = 5
-            self.model = AdolcRosenbrock(n, name='Rosenbrock', x0=-np.ones(n))
-
-    class SparseRosenbrock(SparseAdolcModel, AdolcRosenbrock):
-        pass
-
-    class Test_SparseAdolcRosenbrock(TestCase, Rosenbrock):
-
-        def get_derivatives(self, model):
-            return get_derivatives_coord(model)
-
-        def setUp(self):
-            n = 5
-            self.model = SparseRosenbrock(n, name='Rosenbrock', x0=-np.ones(n))
-
     class SparseHs7(SparseAdolcModel, AdolcHs7):
         pass
 
-    class Test_AdolcHs7(TestCase, Hs7):  # Test def'd in Hs7
+except:
+    adolc_missing = True
 
-        def get_derivatives(self, model):
-            return get_derivatives_plain(model)
 
-        def setUp(self):
-            n = 2
-            m = 1
-            self.model = AdolcHs7(2,
-                                  m=m,
-                                  name='HS7',
-                                  x0=2 * np.ones(n),
-                                  pi0=np.ones(m),
-                                  Lcon=4 * np.ones(m),
-                                  Ucon=4 * np.ones(m))
+try:
+    from nlp.model.adolcmodel import PySparseAdolcModel
 
-    class TestSparseAdolcHs7(TestCase, Hs7):
+    class PySparseRosenbrock(PySparseAdolcModel, AdolcRosenbrock):
+        pass
 
-        def get_derivatives(self, model):
-            return get_derivatives_coord(model)
+    class PySparseHs7(PySparseAdolcModel, AdolcHs7):
+        pass
 
-        def setUp(self):
-            n = 2
-            m = 1
-            self.model = SparseHs7(2,
-                                   m=m,
-                                   name='HS7',
-                                   x0=2 * np.ones(n),
-                                   pi0=np.ones(m),
-                                   Lcon=4 * np.ones(m),
-                                   Ucon=4 * np.ones(m))
+except:
+    pysparse_missing = True
 
-    if not module_missing('pysparse'):
-        from nlp.model.adolcmodel import PySparseAdolcModel
 
-        class PySparseRosenbrock(PySparseAdolcModel, AdolcRosenbrock):
-            pass
+try:
+    from nlp.model.adolcmodel import SciPyAdolcModel
 
-        class Test_PySparseAdolcRosenbrock(TestCase, Rosenbrock):
+    class SciPyRosenbrock(SciPyAdolcModel, AdolcRosenbrock):
+        pass
 
-            def get_derivatives(self, model):
-                return get_derivatives_llmat(model)
+    class SciPyHs7(SciPyAdolcModel, AdolcHs7):
+        pass
 
-            def setUp(self):
-                n = 5
-                self.model = PySparseRosenbrock(n,
-                                                name='Rosenbrock',
-                                                x0=-np.ones(n))
+except:
+    scipy_missing = True
 
-        class PySparseHs7(PySparseAdolcModel, AdolcHs7):
-            pass
 
-        class Test_PySparseAdolcHs7(TestCase, Hs7):
+adolc = pytest.mark.skipif(adolc_missing, reason="requires ADOL-C")
+pysparse = pytest.mark.skipif(pysparse_missing, reason="requires PySparse")
+scipy = pytest.mark.skipif(scipy_missing, reason="requires SciPy")
 
-            def get_derivatives(self, model):
-                return get_derivatives_llmat(model)
 
-            def setUp(self):
-                n = 2
-                m = 1
-                self.model = PySparseHs7(2,
-                                         m=m,
-                                         name='HS7',
-                                         x0=2 * np.ones(n),
-                                         pi0=np.ones(m),
-                                         Lcon=4 * np.ones(m),
-                                         Ucon=4 * np.ones(m))
+@adolc
+class Test_AdolcRosenbrock(TestCase, Rosenbrock):
+    # Test def'd in Rosenbrock
 
-    if not module_missing('scipy'):
-        from nlp.model.adolcmodel import SciPyAdolcModel
+    def get_derivatives(self, model):
+        return get_derivatives_plain(model)
 
-        class SciPyRosenbrock(SciPyAdolcModel, AdolcRosenbrock):
-            pass
+    def setUp(self):
+        n = 5
+        self.model = AdolcRosenbrock(n, name='Rosenbrock', x0=-np.ones(n))
 
-        class Test_SciPyAdolcRosenbrock(TestCase, Rosenbrock):
 
-            def get_derivatives(self, model):
-                return get_derivatives_scipy(model)
+@adolc
+class Test_SparseAdolcRosenbrock(TestCase, Rosenbrock):
 
-            def setUp(self):
-                n = 5
-                self.model = SciPyRosenbrock(n,
-                                             name='Rosenbrock',
-                                             x0=-np.ones(n))
+    def get_derivatives(self, model):
+        return get_derivatives_coord(model)
 
-        class SciPyHs7(SciPyAdolcModel, AdolcHs7):
-            pass
+    def setUp(self):
+        n = 5
+        self.model = SparseRosenbrock(n, name='Rosenbrock', x0=-np.ones(n))
 
-        class Test_SciPyAdolcHs7(TestCase, Hs7):
 
-            def get_derivatives(self, model):
-                return get_derivatives_scipy(model)
+@adolc
+class Test_AdolcHs7(TestCase, Hs7):  # Test def'd in Hs7
 
-            def setUp(self):
-                n = 2
-                m = 1
-                self.model = SciPyHs7(2,
-                                      m=m,
-                                      name='HS7',
-                                      x0=2 * np.ones(n),
-                                      pi0=np.ones(m),
-                                      Lcon=4 * np.ones(m),
-                                      Ucon=4 * np.ones(m))
+    def get_derivatives(self, model):
+        return get_derivatives_plain(model)
+
+    def setUp(self):
+        n = 2
+        m = 1
+        self.model = AdolcHs7(2,
+                              m=m,
+                              name='HS7',
+                              x0=2 * np.ones(n),
+                              pi0=np.ones(m),
+                              Lcon=4 * np.ones(m),
+                              Ucon=4 * np.ones(m))
+
+
+@adolc
+class TestSparseAdolcHs7(TestCase, Hs7):
+
+    def get_derivatives(self, model):
+        return get_derivatives_coord(model)
+
+    def setUp(self):
+        n = 2
+        m = 1
+        self.model = SparseHs7(2,
+                               m=m,
+                               name='HS7',
+                               x0=2 * np.ones(n),
+                               pi0=np.ones(m),
+                               Lcon=4 * np.ones(m),
+                               Ucon=4 * np.ones(m))
+
+
+@pysparse
+class Test_PySparseAdolcRosenbrock(TestCase, Rosenbrock):
+
+    def get_derivatives(self, model):
+        return get_derivatives_llmat(model)
+
+    def setUp(self):
+        n = 5
+        self.model = PySparseRosenbrock(n,
+                                        name='Rosenbrock',
+                                        x0=-np.ones(n))
+
+
+@pysparse
+class Test_PySparseAdolcHs7(TestCase, Hs7):
+
+    def get_derivatives(self, model):
+        return get_derivatives_llmat(model)
+
+    def setUp(self):
+        n = 2
+        m = 1
+        self.model = PySparseHs7(2,
+                                 m=m,
+                                 name='HS7',
+                                 x0=2 * np.ones(n),
+                                 pi0=np.ones(m),
+                                 Lcon=4 * np.ones(m),
+                                 Ucon=4 * np.ones(m))
+
+
+@scipy
+class Test_SciPyAdolcRosenbrock(TestCase, Rosenbrock):
+
+    def get_derivatives(self, model):
+        return get_derivatives_scipy(model)
+
+    def setUp(self):
+        n = 5
+        self.model = SciPyRosenbrock(n,
+                                     name='Rosenbrock',
+                                     x0=-np.ones(n))
+
+
+@scipy
+class Test_SciPyAdolcHs7(TestCase, Hs7):
+
+    def get_derivatives(self, model):
+        return get_derivatives_scipy(model)
+
+    def setUp(self):
+        n = 2
+        m = 1
+        self.model = SciPyHs7(2,
+                              m=m,
+                              name='HS7',
+                              x0=2 * np.ones(n),
+                              pi0=np.ones(m),
+                              Lcon=4 * np.ones(m),
+                              Ucon=4 * np.ones(m))
