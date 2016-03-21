7780c0be10bdd8e3f954f6b0e5e963741eb60b91
Dominique Orban <dominique.orban@gmail.com>
2016-03-21 12:46:08 -0400

    C1 and C2 line models

---
+++ nlp/model/linemodel.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""Restriction of models to lines."""
+
+from nlp.model.nlpmodel import NLPModel
+import numpy as np
+
+
+class C1LineModel(NLPModel):
+    u"""Restriction of a C¹ model to a line.
+
+    An instance of this class is a model representing the original model
+    restricted to the line x + td. More precisely, if the original objective
+    is f: ℝⁿ → ℝ, then, given x ∈ ℝⁿ and d ∈ ℝⁿ (d≠0) a fixed direction, the
+    objective of the restricted model is ϕ: ℝ → ℝ defined by
+
+        ϕ(t) := f(x + td).
+
+    Similarly, if the original constraints are c: ℝⁿ → ℝᵐ, the constraints of
+    the restricted model are γ: ℝ → ℝᵐ defined by
+
+        γ(t) := c(x + td).
+
+    The functions f and c are only assumed to be C¹, i.e., only values and
+    first derivatives of ϕ and γ are defined.
+
+
+        :parameters:
+            :model: ```NLPModel``` whose objective is to be restricted
+            :x: Numpy array
+            :d: Numpy array assumed to be nonzero (no check is performed).
+    """
+
+    def __init__(self, model, x, d):
+        name = "line-" + model.name
+        super(C1LineModel, self).__init__(1,
+                                          m=model.ncon,
+                                          name=name,
+                                          x0=0,
+                                          Lvar=model.Lvar,
+                                          Uvar=model.Uvar,
+                                          Lcon=model.Lcon,
+                                          Ucon=model.Ucon)
+        self.__x = x
+        self.__d = d
+        self.__model = model
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def d(self):
+        return self.__d
+
+    @property
+    def dir(self):
+        return self.__d
+
+    @property
+    def model(self):
+        return self.__model
+
+    def obj(self, t):
+        u"""Evaluate ϕ(t) = f(x + td)."""
+        return self.model.obj(self.x + t * self.d)
+
+    def grad(self, t):
+        u"""Evaluate ϕ'(t) = ∇f(x + td)ᵀ d."""
+        return np.dot(self.model.grad(self.x + t * self.d), self.d)
+
+    def cons(self, t):
+        u"""Evaluate γ(t) = c(x + td)."""
+        return self.model.cons(self.x + t * self.d)
+
+    def jac(self, t):
+        u"""Evaluate γ'(t) = J(x + td) d."""
+        return self.model.jprod(self.x + t * self.d, self.d)
+
+    def jprod(self, t, v):
+        u"""Jacobian-vector product, which is just γ'(t)*v with v ∈ ℝ."""
+        return self.jac(t) * v
+
+    def jtprod(self, t, u):
+        u"""Transposed-Jacobian-vector product γ'(t)ᵀ u with u ∈ ℝᵐ."""
+        return np.dot(self.jac(t), u)
+
+
+class C2LineModel(C1LineModel):
+    u"""Restriction of a C² objective function to a line.
+
+    If f: ℝⁿ → ℝ, x ∈ ℝⁿ and d ∈ ℝⁿ (d≠0) is a fixed direction, an instance
+    of this class is a model representing the function f restricted to
+    the line x + td, i.e., the function ϕ: ℝ → ℝ defined by
+
+        ϕ(t) := f(x + td).
+
+    The function f is assumed to be C², i.e., values and first and second
+    derivatives of ϕ are defined.
+
+        :parameters:
+            :model: ```NLPModel``` whose objective is to be restricted
+            :x: Numpy array
+            :d: Numpy array assumed to be nonzero (no check is performed).
+    """
+
+    def hess(self, t, z):
+        u"""Evaluate ϕ"(t) = dᵀ ∇²L(x + td, z) d."""
+        return np.dot(self.d, self.model.hprod(self.x + t * self.d, z, self.d))
+
+    def hprod(self, t, z, v):
+        u"""Hessian-vector product, which is just ϕ"(t)*v with v ∈ ℝ."""
+        return self.hess(t, z) * v
---
+++ tests/model/test_linemodel.py
@@ -0,0 +1,100 @@
+from nlp.model.nlpmodel import NLPModel, UnconstrainedNLPModel
+from nlp.model.linemodel import C1LineModel, C2LineModel
+
+import numpy as np
+import pytest
+
+
+class Rosenbrock(UnconstrainedNLPModel):
+    def __init__(self, nvar, **kwargs):
+        assert (nvar > 1)
+        super(Rosenbrock, self).__init__(nvar, **kwargs)
+
+    def obj(self, x):
+        return np.sum(100 * (x[1:] - x[:-1]**2)**2 + (1 - x[:-1])**2)
+
+    def grad(self, x):
+        n = self.nvar
+        g = np.empty(n)
+        g[0] = -400 * x[0] * (x[1] - x[0]**2) - 2 * (1 - x[0])
+        g[-1] = 200 * (x[-1] - x[-2]**2)
+        g[1:-1] = 200 * (x[1:-1] - x[:-2]**2) - \
+                  400 * x[1:-1] * (x[2:] - x[1:-1]**2) - 2 * (1 - x[1:-1])
+        return g
+
+    def diags(self, x):
+        n = self.nvar
+        d = np.empty(n)
+        d[:-1] = 800 * x[:-1]**2 - 400 * (x[1:] - x[:-1]**2) + 2
+        d[1:] += 200
+        d[-1] = 200
+        o = -400 * x[:-1]
+        return (d, o)
+
+    def hess(self, x, z):
+        (d, o) = self.diags(x)
+        return np.diag(d) + np.diag(o, 1) + np.diag(o, -1)
+
+    def hprod(self, x, z, v):
+        (d, o) = self.diags(x)
+        hv = d * v
+        hv[1:] += o * x[1:]
+        hv[:-1] += o * x[:-1]
+        return hv
+
+
+@pytest.fixture(params=[2, 5, 10])
+def c1rosenbrock_restriction(request):
+    return C1LineModel(Rosenbrock(request.param),
+                       np.zeros(request.param),
+                       np.ones(request.param))
+
+
+def test_c1rosenbrock(c1rosenbrock_restriction):
+    model = c1rosenbrock_restriction.model
+    x = np.zeros(model.nvar)
+    d = np.ones(model.nvar)
+    assert (c1rosenbrock_restriction.nvar == 1)
+    assert (c1rosenbrock_restriction.ncon == 0)
+    assert np.allclose(c1rosenbrock_restriction.obj(0), model.obj(x))
+    assert np.allclose(c1rosenbrock_restriction.obj(1), model.obj(x + d))
+
+    assert np.allclose(c1rosenbrock_restriction.grad(0),
+                       np.dot(model.grad(x), d))
+    assert np.allclose(c1rosenbrock_restriction.grad(1),
+                       np.dot(model.grad(x + d), d))
+
+    with pytest.raises(NotImplementedError):
+        c1rosenbrock_restriction.hess(0)
+
+    with pytest.raises(NotImplementedError):
+        c1rosenbrock_restriction.hprod(0, 0, 2)
+
+@pytest.fixture(params=[2, 5, 10])
+def c2rosenbrock_restriction(request):
+    return C2LineModel(Rosenbrock(request.param),
+                       np.zeros(request.param),
+                       np.ones(request.param))
+
+
+def test_c2rosenbrock(c2rosenbrock_restriction):
+    model = c2rosenbrock_restriction.model
+    x = np.zeros(model.nvar)
+    d = np.ones(model.nvar)
+    assert (c2rosenbrock_restriction.nvar == 1)
+    assert (c2rosenbrock_restriction.ncon == 0)
+    assert np.allclose(c2rosenbrock_restriction.obj(0), model.obj(x))
+    assert np.allclose(c2rosenbrock_restriction.obj(1), model.obj(x + d))
+
+    assert np.allclose(c2rosenbrock_restriction.grad(0),
+                       np.dot(model.grad(x), d))
+    assert np.allclose(c2rosenbrock_restriction.grad(1),
+                       np.dot(model.grad(x + d), d))
+
+    assert np.allclose(c2rosenbrock_restriction.hess(0, 0),
+                       np.dot(d, model.hprod(x, 0, d)))
+    assert np.allclose(c2rosenbrock_restriction.hess(1, 0),
+                       np.dot(d, model.hprod(x + d, 0, d)))
+
+    assert np.allclose(c2rosenbrock_restriction.hprod(0, 0, 2),
+                       2 * np.dot(d, model.hprod(x, 0, d)))
