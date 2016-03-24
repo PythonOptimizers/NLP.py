f66baf377ba508706d165e410381fcf808e172b3
Dominique Orban <dominique.orban@gmail.com>
2016-03-24 08:33:15 -0400

    lbfgs ampl driver

---
+++ nlp/drivers/nlp_lbfgs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""Simple AMPL driver for L-BFGS."""
+
+from nlp.model.amplpy import QNAmplModel
+from nlp.optimize.lbfgs import LBFGSFramework
+from nlp.tools.logs import config_logger
+
+from pykrylov.linop import InverseLBFGSOperator
+import sys
+
+if len(sys.argv) == 1:
+    raise ValueError("Please supply problem name as argument")
+
+# Create root logger.
+log = config_logger("nlp.lbfgs", "%(name)-9s %(levelname)-5s %(message)s")
+
+model = QNAmplModel(sys.argv[1], H=InverseLBFGSOperator, scaling=True)
+lbfgs = LBFGSFramework(model)
+lbfgs.solve()
