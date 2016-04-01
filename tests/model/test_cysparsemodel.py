0f505c199511cc734128aba4a9145e984e57e035
Dominique Orban <dominique.orban@gmail.com>
2016-03-31 20:26:04 -0400

    disable check in cysparsemodel tests for now

--- tests/model/test_cysparsemodel.py
+++ tests/model/test_cysparsemodel.py
@@ -4,50 +4,50 @@ from unittest import TestCase
 from helper import *
 import numpy as np
 import os
+from nlp.model.cysparsemodel import CySparseAmplModel, CySparseSlackModel
+
 
 this_path = os.path.dirname(os.path.realpath(__file__))
 
-if not module_missing('cysparse'):
-    from nlp.model.cysparsemodel import CySparseAmplModel, CySparseSlackModel
 
-    class Test_CySparseAmplRosenbrock(TestCase, Rosenbrock):
-        def get_derivatives(self, model):
-            return get_derivatives_llmat(model)
+class Test_CySparseAmplRosenbrock(TestCase, Rosenbrock):
+    def get_derivatives(self, model):
+        return get_derivatives_llmat(model)
 
-        def setUp(self):
-            model = os.path.join(this_path, 'rosenbrock.nl')
-            self.model = CySparseAmplModel(model)  # x0 = (-1, ..., -1)
+    def setUp(self):
+        model = os.path.join(this_path, 'rosenbrock.nl')
+        self.model = CySparseAmplModel(model)  # x0 = (-1, ..., -1)
 
-    class Test_CySparseAmplHS7(TestCase, Hs7):
-        def get_derivatives(self, model):
-            return get_derivatives_llmat(model)
+class Test_CySparseAmplHS7(TestCase, Hs7):
+    def get_derivatives(self, model):
+        return get_derivatives_llmat(model)
 
-        def setUp(self):
-            model = os.path.join(this_path, 'hs007.nl')
-            self.model = CySparseAmplModel(model)
-            self.model.pi0 = np.ones(1)
+    def setUp(self):
+        model = os.path.join(this_path, 'hs007.nl')
+        self.model = CySparseAmplModel(model)
+        self.model.pi0 = np.ones(1)
 
-    class Test_CySparseSlackHS7(TestCase, GenericTest):
-        def get_expected(self):
-            return Hs7SlackData()
+class Test_CySparseSlackHS7(TestCase, GenericTest):
+    def get_expected(self):
+        return Hs7SlackData()
 
-        def get_derivatives(self, model):
-            return get_derivatives_llmat(model)
+    def get_derivatives(self, model):
+        return get_derivatives_llmat(model)
 
-        def setUp(self):
-            model = os.path.join(this_path, 'hs007.nl')
-            self.model = CySparseSlackModel(CySparseAmplModel(model))
-            self.model.pi0 = np.ones(1)
+    def setUp(self):
+        model = os.path.join(this_path, 'hs007.nl')
+        self.model = CySparseSlackModel(CySparseAmplModel(model))
+        self.model.pi0 = np.ones(1)
 
-    class Test_CySparseSlackHS10(TestCase, GenericTest):
-        def get_expected(self):
-            return Hs10SlackData()
+class Test_CySparseSlackHS10(TestCase, GenericTest):
+    def get_expected(self):
+        return Hs10SlackData()
 
-        def get_derivatives(self, model):
-            return get_derivatives_llmat(model)
+    def get_derivatives(self, model):
+        return get_derivatives_llmat(model)
 
-        def setUp(self):
-            model = os.path.join(this_path, 'hs010.nl')
-            self.model = CySparseSlackModel(CySparseAmplModel(model))
-            self.model.pi0 = np.ones(2)
-            self.model.x0[2] = 1
+    def setUp(self):
+        model = os.path.join(this_path, 'hs010.nl')
+        self.model = CySparseSlackModel(CySparseAmplModel(model))
+        self.model.pi0 = np.ones(2)
+        self.model.x0[2] = 1
