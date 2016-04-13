6df07087970dd7307ff0ac41e1ce5a7bfc91c8c1
Dominique Orban <dominique.orban@gmail.com>
2016-04-13 16:28:25 -0400

    test_tron is unrelated to TRON!!!

--- tests/optimize/test_tron.py
+++ tests/optimize/test_tron.py
@@ -1,80 +1,67 @@
-"""Tests relative to TRON."""
-
-from helper import *
-from nlp.optimize.tron import TRON
-
 import numpy as np
-import pytest
-import os
-
-this_path = os.path.dirname(os.path.realpath(__file__))
+from nlp.tools.utils import breakpoints
 
 
 def test_breakpoints():
-    """Test :meth: breakpoints from :class: TRON."""
-    pytest.importorskip("nlp.model.amplpy")
-    model = os.path.join(this_path, "..", "model", "rosenbrock.nl")
-    return TRON(AmplModel(model))
-
     xl = np.array([-3, -2])
     xu = np.array([2, 1])
 
-    # x is strictly inside boundaries
+    # x is strictly inside the bounds
     x = np.array([-2, -1])
     d = np.array([-1, 1])
-    (nbrpt, brptmin, brptmax) = self.tron.breakpoints(x, d, xl, xu)
-    assert(nbrpt == 2)
-    assert(brptmin == 1)
-    assert(brptmax == 2)
+    (nbrpt, brptmin, brptmax) = breakpoints(x, d, xl, xu)
+    assert nbrpt == 2
+    assert brptmin == 1
+    assert brptmax == 2
 
     d = np.array([2, 1])
-    (nbrpt, brptmin, brptmax) = self.tron.breakpoints(x, d, xl, xu)
-    assert(nbrpt == 2)
-    assert(brptmin == 2)
-    assert(brptmax == 2)
+    (nbrpt, brptmin, brptmax) = breakpoints(x, d, xl, xu)
+    assert nbrpt == 2
+    assert brptmin == 2
+    assert brptmax == 2
 
     d = np.array([2, -1])
-    (nbrpt, brptmin, brptmax) = self.tron.breakpoints(x, d, xl, xu)
-    assert(nbrpt == 2)
-    assert(brptmin == 1)
-    assert(brptmax == 2)
+    (nbrpt, brptmin, brptmax) = breakpoints(x, d, xl, xu)
+    assert nbrpt == 2
+    assert brptmin == 1
+    assert brptmax == 2
 
     d = np.array([-1, -1])
-    (nbrpt, brptmin, brptmax) = self.tron.breakpoints(x, d, xl, xu)
-    assert(nbrpt == 2)
-    assert(brptmin == 1)
-    assert(brptmax == 1)
+    (nbrpt, brptmin, brptmax) = breakpoints(x, d, xl, xu)
+    assert nbrpt == 2
+    assert brptmin == 1
+    assert brptmax == 1
 
     d = np.array([0, 0])
-    (nbrpt, brptmin, brptmax) = self.tron.breakpoints(x, d, xl, xu)
-    assert(nbrpt == 0)
-    assert(brptmin == 0)
-    assert(brptmax == 0)
+    (nbrpt, brptmin, brptmax) = breakpoints(x, d, xl, xu)
+    assert nbrpt == 0
+    assert brptmin == 0
+    assert brptmax == 0
 
-    # x is on both boundaries
+    # two bounds are active at x
     x = np.array([2, 1])
     d = np.array([-1, -1])
-    (nbrpt, brptmin, brptmax) = self.tron.breakpoints(x, d, xl, xu)
-    assert(nbrpt == 2)
-    assert(brptmin == 3)
-    assert(brptmax == 5)
+    (nbrpt, brptmin, brptmax) = breakpoints(x, d, xl, xu)
+    assert nbrpt == 2
+    assert brptmin == 3
+    assert brptmax == 5
 
     d = np.array([1, 1])
-    (nbrpt, brptmin, brptmax) = self.tron.breakpoints(x, d, xl, xu)
-    assert(nbrpt == 0)
-    assert(brptmin == 0)
-    assert(brptmax == 0)
+    (nbrpt, brptmin, brptmax) = breakpoints(x, d, xl, xu)
+    assert nbrpt == 0
+    assert brptmin == 0
+    assert brptmax == 0
 
     d = np.array([1, 1])
-    (nbrpt, brptmin, brptmax) = self.tron.breakpoints(x, d, xl, xu)
-    assert(nbrpt == 0)
-    assert(brptmin == 0)
-    assert(brptmax == 0)
+    (nbrpt, brptmin, brptmax) = breakpoints(x, d, xl, xu)
+    assert nbrpt == 0
+    assert brptmin == 0
+    assert brptmax == 0
 
-    # x is on one boundary
+    # one bound is active at x
     x = np.array([-1, 1])
     d = np.array([1, 2])
-    (nbrpt, brptmin, brptmax) = self.tron.breakpoints(x, d, xl, xu)
-    assert(nbrpt == 1)
-    assert(brptmin == 3)
-    assert(brptmax == 3)
+    (nbrpt, brptmin, brptmax) = breakpoints(x, d, xl, xu)
+    assert nbrpt == 1
+    assert brptmin == 3
+    assert brptmax == 3
