d371d992bc3c1cede3cb9a145a4d060a1d094f34
Dominique Orban <dominique.orban@gmail.com>
2016-04-17 01:18:32 -0400

    lbfgs driver: modernize, use wolfe ls

--- nlp/drivers/nlp_lbfgs.py
+++ nlp/drivers/nlp_lbfgs.py
@@ -5,10 +5,26 @@
 import logging
 import sys
 from nlp.model.amplmodel import QNAmplModel
-from nlp.optimize.lbfgs import LBFGS
+from nlp.optimize.lbfgs import LBFGS, WolfeLBFGS
 from nlp.tools.logs import config_logger
 
 from pykrylov.linop import InverseLBFGSOperator
+
+
+def lbfgs_stats(lbfgs):
+    """Obtain L-BFGS statistics and indicate failures with negatives."""
+    if lbfgs.status == "opt":
+        it = lbfgs.iter
+        fc, gc = lbfgs.model.obj.ncalls, lbfgs.model.grad.ncalls
+        gn = lbfgs.g_norm
+        ts = lbfgs.tsolve
+    else:
+        it = -lbfgs.iter
+        fc, gc = -lbfgs.model.obj.ncalls, -lbfgs.model.grad.ncalls
+        gn = -1.0 if lbfgs.g_norm is None else -lbfgs.g_norm
+        ts = -1.0 if lbfgs.tsolve is None else -lbfgs.tsolve
+    return (it, fc, gc, gn, ts)
+
 
 nprobs = len(sys.argv) - 1
 if nprobs == 0:
@@ -23,13 +39,23 @@ slv_log = config_logger("nlp.lbfgs",
                         "%(name)-9s %(levelname)-5s %(message)s",
                         level=logging.WARN if nprobs > 1 else logging.INFO)
 
-logger.info("%10s %5s %8s %7s %5s %5s %4s %s",
-            "name", "nvar", "f", u"‖∇f‖", "#f", u"#∇f", "stat", "time")
+logger.info("%10s %5s %6s %8s %8s %6s %6s %5s %7s",
+            "name", "nvar", "iter", "f", u"‖∇f‖", "#f", u"#∇f", "stat", "time")
+
 for problem in sys.argv[1:]:
     model = QNAmplModel(problem, H=InverseLBFGSOperator, scaling=True)
-    lbfgs = LBFGS(model, maxiter=300)
-    lbfgs.solve()
-    logger.info("%10s %5d %8.1e %7.1e %5d %5d %4s %.3f",
-                model.name, model.nvar, lbfgs.f, lbfgs.gNorm,
-                model.obj.ncalls, model.grad.ncalls,
-                lbfgs.status, lbfgs.tsolve)
+    model.compute_scaling_obj()
+
+    lbfgs = WolfeLBFGS(model, maxiter=300)
+    try:
+        lbfgs.solve()
+        status = lbfgs.status
+        niter, fcalls, gcalls, gnorm, tsolve = lbfgs_stats(lbfgs)
+    except:
+        msg = sys.exc_info()[1].message
+        status = msg if len(msg) > 0 else "xfail"  # unknown failure
+        niter, fcalls, gcalls, gnorm, tsolve = lbfgs_stats(lbfgs)
+
+    logger.info("%10s %5d %6d %8.1e %8.1e %6d %6d %5s %7.3f",
+                model.name, model.nvar, niter, lbfgs.f, gnorm,
+                fcalls, gcalls, status, tsolve)
