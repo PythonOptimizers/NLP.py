6c8ef09108b5417afa068463ac063042e41ff028
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2016-05-13 22:13:24 -0400

    fix a bug due to AMPL (see docstring of AmplModel hprod method).

--- nlp/optimize/tron.py
+++ nlp/optimize/tron.py
@@ -418,7 +418,7 @@ class TRON(object):
                 self.g_old = self.g.copy()
 
             # Wrap Hessian into an operator.
-            H = model.hop(self.x, self.model.pi0)
+            H = model.hop(self.x.copy(), self.model.pi0)
 
             # Compute the Cauchy step and store in s.
             (s, self.alphac) = self.cauchy(self.x, self.g, H,
