fix scaling.

nlp/model/amplmodel.py
@@ -5,7 +5,7 @@
 """
 try:
     from nlp.model import _amplmodel
-except:
+except ImportError:
     raise
 
 import numpy as np
@@ -125,6 +125,9 @@ class AmplModel(NLPModel):
     def writesol(self, x, z, msg):
         """Write primal-dual solution and message msg to `stub.sol`."""
         return self.model.ampl_sol(x, z, msg)
+
+    def get_pi0(self):
+        return self.model.pi0()
 
     def obj(self, x, obj_num=0):
         """Evaluate objective function value at x.
@@ -347,9 +350,15 @@ class AmplModel(NLPModel):
         store_zeros = 1 if store_zeros else 0
         if z is None:
             z = np.zeros(self.m)
-        vals, rows, cols = self.model.eval_H(x, z, obj_weight, store_zeros)
+
         if self.scale_obj:
-            vals *= self.scale_obj
+            obj_weight *= self.scale_obj
+        if self.scale_con:
+            z = z.copy()
+            z *= self.scale_con
+
+        vals, rows, cols = self.model.eval_H(x, z, obj_weight, store_zeros)
+
         if not self.minimize:
             vals *= -1
         return (vals, rows, cols)
@@ -375,9 +384,14 @@ class AmplModel(NLPModel):
         obj_weight = kwargs.get('obj_weight', 1.0)
         if z is None:
             z = np.zeros(self.m)
-        Hv = self.model.H_prod(z, v, obj_weight)
+
         if self.scale_obj:
-            Hv *= self.scale_obj
+            obj_weight *= self.scale_obj
+        if self.scale_con:
+            z = z.copy()
+            z *= self.scale_con
+
+        Hv = self.model.H_prod(z, v, obj_weight)
         if not self.minimize:
             Hv *= -1
         return Hv

nlp/model/nlpmodel.py
@@ -294,7 +294,7 @@ class NLPModel(object):
         # Remove scaling if requested
         if reset:
             self.scale_obj = None
-            self.pi0 = self.model.get_pi0()  # get original multipliers
+            # self.pi0 = self.get_pi0()  # get original multipliers
             return
 
         # Quick return if the problem is already scaled
@@ -308,7 +308,7 @@ class NLPModel(object):
         self.scale_obj = g_max / max(g_max, gNorm)  # <= 1 always
 
         # Rescale the Lagrange multiplier
-        self.pi0 *= self.scale_obj
+        # self.pi0 *= self.scale_obj
 
         return gNorm
 
@@ -343,12 +343,12 @@ class NLPModel(object):
         J = self.jop(x)
 
         # Find inf-norm of each row of J
-        gmaxNorm = 0            # holds the maxiumum row-norm of J
+        gmaxNorm = 0            # holds the maximum row-norm of J
         imaxNorm = 0            # holds the corresponding index
         e = np.zeros(self.ncon)
         for i in xrange(m):
             e[i] = 1
-            giNorm = np.linalg.norm(J.T * e, 1)  # Matrix 1-norm (max abs col)
+            giNorm = np.linalg.norm(J.T * e, np.inf)
             e[i] = 0
             d_c[i] = g_max / max(g_max, giNorm)  # <= 1 always
             if giNorm > gmaxNorm: