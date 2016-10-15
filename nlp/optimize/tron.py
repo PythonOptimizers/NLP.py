35231dcddf2420ebafde82b5ca73fcc696d4dee3
Dominique Orban <dominique.orban@gmail.com>
2016-10-15 01:05:09 -0400

    tron: redefine H at each iteration

--- nlp/optimize/tron.py
+++ nlp/optimize/tron.py
@@ -400,8 +400,6 @@ class TRON(object):
         exitFunCall = model.obj.ncalls >= self.maxfuncall
         status = ""
 
-        # Wrap Hessian into an operator.
-        H = model.hop(self.x, self.model.pi0)
         tick = cputime()
 
         # Print out header and initial log.
@@ -416,6 +414,9 @@ class TRON(object):
             self.step_accepted = False
             if self.save_g:
                 self.g_old = self.g.copy()
+
+            # Wrap Hessian into an operator.
+            H = model.hop(self.x, self.model.pi0)
 
             # Compute the Cauchy step and store in s.
             (s, self.alphac) = self.cauchy(self.x, self.g, H,
