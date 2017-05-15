42fa6810c8461a97ff8e4914ea76f53fe2ab4405
Sylvain <sylvain.arreckx@gmail.com>
2017-05-15 23:51:35 +0200

    Example that creates a NL file from a Pyomo model (#82)
    * Pyomo example.
    * fix travis script.
    * new example with pyomo and TRON.

--- nlp/optimize/tron.py
+++ nlp/optimize/tron.py
@@ -426,7 +426,7 @@ class TRON(object):
                 self.g_old = self.g.copy()
 
             # Wrap Hessian into an operator.
-            H = model.hop(self.x.copy(), self.model.pi)
+            H = model.hop(self.x.copy())
 
             # Compute the Cauchy step and store in s.
             (s, self.alphac) = self.cauchy(self.x, self.g, H,
